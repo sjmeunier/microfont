677824c1456dffdd353ebd99d8acf9d1fb5897c3
Serge Meunier <serge@pubble.nl>
2020-05-12 09:45:50 +0200

    Code cleanup

--- MicroFont/MicroFont.cs
+++ MicroFont/MicroFont.cs
@@ -5,48 +5,46 @@ namespace MicroFont
 {
     public class MicroFont
     {
-        private FontDefinition font = new FontDefinition();
-
         public MicroFont()
         {
 
         }
 
-		public Size MeasureText(string text, int xPadding, int yPadding)
-		{
-			Size size = new Size(xPadding, yPadding);
+        public Size MeasureText(string text, int xPadding, int yPadding)
+        {
+            Size size = new Size(xPadding, yPadding);
 
-			int sizeX = xPadding;
+            int sizeX = xPadding;
 
-			foreach (char character in text.ToUpper())
-			{
-				if (character == '\n')
-				{
-					size.Height += 6;
-					size.Width = Math.Max(size.Width, sizeX);
-					sizeX = xPadding;
-					continue;
-				}
-				if (font.Characters.ContainsKey(character))
-				{
-					int[,] fontChar = font.Characters[character];
-					sizeX += 2;
-				}
+            foreach (char character in text.ToUpper())
+            {
+                if (character == '\n')
+                {
+                    size.Height += 6;
+                    size.Width = Math.Max(size.Width, sizeX);
+                    sizeX = xPadding;
+                    continue;
+                }
+                if (MicroFontDefinition.Characters.ContainsKey(character))
+                {
+                    int[,] fontChar = MicroFontDefinition.Characters[character];
+                    sizeX += 2;
+                }
 
-			}
-			size.Width = Math.Max(size.Width, sizeX) + xPadding - 1;
-			size.Height += 5 + yPadding;
+            }
+            size.Width = Math.Max(size.Width, sizeX) + xPadding - 1;
+            size.Height += 5 + yPadding;
 
-			return size;
-		}
+            return size;
+        }
 
-		public Bitmap Generate(string text, int xPadding, int yPadding, bool invertColours)
-		{
-			Size size = MeasureText(text, xPadding, yPadding);
-			return Generate(text, size.Width, size.Height, xPadding, yPadding, invertColours);
-		}
+        public Bitmap Generate(string text, int xPadding, int yPadding, bool invertColours)
+        {
+            Size size = MeasureText(text, xPadding, yPadding);
+            return Generate(text, size.Width, size.Height, xPadding, yPadding, invertColours);
+        }
 
-		public Bitmap Generate(string text, int width, int height, int xPadding, int yPadding, bool invertColours)
+        public Bitmap Generate(string text, int width, int height, int xPadding, int yPadding, bool invertColours)
         {
             Bitmap image = new Bitmap(width, height);
             Graphics g = Graphics.FromImage(image);
@@ -54,9 +52,9 @@ namespace MicroFont
 
             int curX = xPadding;
             int curY = yPadding;
-			
 
-            foreach(char character in text.ToUpper())
+
+            foreach (char character in text.ToUpper())
             {
                 if (character == '\n')
                 {
@@ -64,16 +62,17 @@ namespace MicroFont
                     curX = xPadding;
                     continue;
                 }
-                if (font.Characters.ContainsKey(character)) {
-                    int[,] fontChar = font.Characters[character];
-                    for(int i = 0; i < 5; i++)
+                if (MicroFontDefinition.Characters.ContainsKey(character))
+                {
+                    int[,] fontChar = MicroFontDefinition.Characters[character];
+                    for (int i = 0; i < 5; i++)
                     {
-						if (invertColours)
-							image.SetPixel(curX, curY + i, Color.FromArgb(255 * (1 - fontChar[i, 0]), 255 * (1 - fontChar[i, 1]), 255 * (1 - fontChar[i, 2])));
-						else
-							image.SetPixel(curX, curY + i, Color.FromArgb(255 * fontChar[i, 0], 255 * fontChar[i, 1], 255 * fontChar[i, 2]));
-					}
-					curX += 2;
+                        if (invertColours)
+                            image.SetPixel(curX, curY + i, Color.FromArgb(255 * (1 - fontChar[i, 0]), 255 * (1 - fontChar[i, 1]), 255 * (1 - fontChar[i, 2])));
+                        else
+                            image.SetPixel(curX, curY + i, Color.FromArgb(255 * fontChar[i, 0], 255 * fontChar[i, 1], 255 * fontChar[i, 2]));
+                    }
+                    curX += 2;
                 }
 
             }
